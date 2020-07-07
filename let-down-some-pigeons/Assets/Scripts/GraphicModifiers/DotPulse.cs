a422d747369d09b3b3a0e68e46d2270ae8a2a7ea
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-07-07 19:59:46 +0500

    New menu and level menu

--- let-down-some-pigeons/Assets/Scripts/GraphicModifiers/DotPulse.cs
+++ let-down-some-pigeons/Assets/Scripts/GraphicModifiers/DotPulse.cs
@@ -7,18 +7,23 @@ public class DotPulse : MonoBehaviour {
     private float timeFromStart;
     [Range(0f, 0.1f)] public float scalingPower = 0f;
     [Range(0f, 0.1f)] public float movingPower = 0.0055f;
+    public bool unlockMoving = false;
     public bool moveRandomly = true;
+    public bool useY = true;
+    public int iterationsMax = 37;
     private Vector3 lastChoice;
     private int moveInterations;
 
-    private void Start() => movingPower = 0.0055f;
-
+    private void Start(){ 
+        if (!unlockMoving) movingPower = 0.0055f;
+    }
     private void FixedUpdate(){
         timeFromStart += Time.deltaTime;
         transform.localScale += new Vector3(1f, 1f, 0f) * Mathf.Sin(timeFromStart) * scalingPower; 
         if (moveRandomly){
-            if (moveInterations % 37 == 0)
+            if (moveInterations % iterationsMax == 0)
                 lastChoice = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f) * movingPower;
+            if (!useY) lastChoice.y = 0f;
             transform.position += lastChoice;
             moveInterations += 1;
         }
